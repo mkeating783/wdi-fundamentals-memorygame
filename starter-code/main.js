8de5b01f1cc8774c0cb378e1f35d94ae93056c8d
A <A@As-MacBook-Pro.local>
2017-01-07 18:16:10 -0500

    Final Memory Card

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,7 +1,7 @@
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
+// var cardOne = 'queen';
+// var cardTwo = 'queen';
+// var cardThree = 'king';
+// var cardFour = 'king';
 
 /* if (cardOne === cardTwo) {
 	alert('You found a match!');	
@@ -10,6 +10,11 @@ var cardFour = 'king';
 } else {
   alert('Sorry, try again.');
 } */
+
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [];
+
 
 var board = document.getElementById('game-board');
 
@@ -25,5 +30,32 @@ function createBoard() {
   }
 
 }
+
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='http://i.imgur.com/UWcLMfO.jpg' alt = King of Clubs>";
+	} else {
+		this.innerHTML = "<img src='http://i.imgur.com/d67h8J9.jpg' alt = Queen of Hearts>"; 
+	}
+
+	  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+}
+
+function isMatch(cards) {
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+
+  }
+}
+
+createBoard();
+
 
 
