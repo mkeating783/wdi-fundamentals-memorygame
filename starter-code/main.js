5df1103715e7b147b7ba816dbeabdf1b939d69c2
A <A@As-MacBook-Pro.local>
2017-01-07 16:16:02 -0500

    Added Cards With JavaScript

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,10 +3,27 @@ var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
 
-if (cardOne === cardTwo) {
+/* if (cardOne === cardTwo) {
 	alert('You found a match!');	
 } else if (cardThree === cardFour) {
 	alert('You found a match!');
 } else {
   alert('Sorry, try again.');
+} */
+
+var board = document.getElementById('game-board');
+
+function createBoard() {
+	for (var i=0; i<cards.length; i++) {
+		    var cardElement = document.createElement('div');
+		     cardElement.className = 'card';
+		     cardElement.setAttribute('data-card', cards[i]);
+		     cardElement.addEventListener('click', isTwoCards);
+
+	board.appendChild(cardElement);
+    board.appendChild(cardElement);
+  }
+
 }
+
+
